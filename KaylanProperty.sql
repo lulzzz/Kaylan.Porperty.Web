9bf6084a3a388c950f44071f2d3e378dc54bcd5f
harshal bhagat <harshalsbhagat@gmail.com>
2017-05-23 01:03:04 +0530

    Database Script Added.

---
+++ KaylanProperty.sql
@@ -0,0 +1,524 @@
+﻿USE [KalyanProperty]
+GO
+/****** Object:  Table [dbo].[__MigrationHistory]    Script Date: 5/23/2017 1:01:22 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[__MigrationHistory](
+	[MigrationId] [nvarchar](150) NOT NULL,
+	[ContextKey] [nvarchar](300) NOT NULL,
+	[Model] [varbinary](max) NOT NULL,
+	[ProductVersion] [nvarchar](32) NOT NULL,
+ CONSTRAINT [PK_dbo.__MigrationHistory] PRIMARY KEY CLUSTERED 
+(
+	[MigrationId] ASC,
+	[ContextKey] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+GO
+/****** Object:  Table [dbo].[AgentInfo]    Script Date: 5/23/2017 1:01:23 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[AgentInfo](
+	[id] [int] NOT NULL,
+	[PropertyDetailId] [int] NOT NULL,
+	[FirstName] [nvarchar](50) NULL,
+	[LastName] [nvarchar](50) NULL,
+	[Phone] [nchar](10) NULL,
+	[IsActive] [bit] NULL,
+	[Address1] [nvarchar](50) NULL,
+	[Address2] [nvarchar](50) NULL,
+	[City] [nchar](10) NULL,
+	[State] [nchar](10) NULL,
+ CONSTRAINT [PK_dbo.AgentInfo] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Amenity]    Script Date: 5/23/2017 1:01:23 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Amenity](
+	[Id] [int] NOT NULL,
+	[ServiceId] [int] NOT NULL,
+	[FeatureId] [int] NOT NULL,
+ CONSTRAINT [PK_dbo.Amenity] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Area]    Script Date: 5/23/2017 1:01:23 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Area](
+	[Id] [int] NOT NULL,
+	[Name] [nvarchar](50) NULL,
+	[IsActive] [bit] NULL,
+	[DistrictId] [int] NULL,
+ CONSTRAINT [PK_dbo.Area] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[City]    Script Date: 5/23/2017 1:01:23 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[City](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NULL,
+	[IsActive] [nvarchar](50) NULL,
+	[StateId] [int] NOT NULL,
+ CONSTRAINT [PK_dbo.City] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Contact]    Script Date: 5/23/2017 1:01:23 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[Contact](
+	[Id] [int] NOT NULL,
+	[Name] [varchar](100) NOT NULL,
+	[Email] [varchar](50) NOT NULL,
+	[Phone] [int] NOT NULL,
+	[CreatedDate] [datetime] NOT NULL,
+	[CreatedBy] [nchar](10) NULL,
+	[Message] [nvarchar](max) NOT NULL,
+	[ContactTypeId] [int] NOT NULL,
+ CONSTRAINT [PK_dbo.Contact] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+GO
+/****** Object:  Table [dbo].[ContractType]    Script Date: 5/23/2017 1:01:23 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ContractType](
+	[Id] [int] NOT NULL,
+	[Conmmertial] [nvarchar](50) NULL,
+	[Date] [datetime] NULL,
+	[Rent] [nvarchar](50) NULL,
+	[Sale] [nvarchar](50) NULL,
+ CONSTRAINT [PK_dbo.ContractType] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Country]    Script Date: 5/23/2017 1:01:23 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Country](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [int] NOT NULL,
+	[IsActive] [bit] NULL,
+ CONSTRAINT [PK_dbo.Country] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[District]    Script Date: 5/23/2017 1:01:23 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[District](
+	[Id] [int] NOT NULL,
+	[Name] [nvarchar](50) NULL,
+	[IsActive] [bit] NULL,
+	[CityId] [int] NULL,
+ CONSTRAINT [PK_dbo.District] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Feature]    Script Date: 5/23/2017 1:01:23 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Feature](
+	[Id] [int] NOT NULL,
+	[Name] [nvarchar](50) NULL,
+ CONSTRAINT [PK_dbo.Feature] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[LoginPl]    Script Date: 5/23/2017 1:01:23 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[LoginPl](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Password] [nvarchar](50) NOT NULL,
+	[Email] [nvarchar](50) NOT NULL,
+	[Date] [datetime] NOT NULL,
+	[LoginId] [nvarchar](50) NULL,
+ CONSTRAINT [PK_dbo.LoginPl] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[PropertyDetail]    Script Date: 5/23/2017 1:01:23 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[PropertyDetail](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[ContractType] [nvarchar](50) NULL,
+	[FromPrice] [nvarchar](50) NULL,
+	[Bedroom] [int] NULL,
+	[Bathroom] [int] NULL,
+	[Parking] [int] NULL,
+	[AmenitiesID] [int] NULL,
+	[Date] [datetime] NULL,
+	[PropertyName] [nvarchar](50) NULL,
+	[Address] [nvarchar](50) NULL,
+	[PropertyDescription] [nvarchar](50) NULL,
+	[FullName] [nvarchar](50) NULL,
+	[Email] [nvarchar](50) NULL,
+	[Phone] [nvarchar](50) NULL,
+	[Comments] [nvarchar](50) NULL,
+	[Images] [nvarchar](50) NULL,
+	[Approved] [varchar](1) NULL,
+	[Featured] [varchar](1) NULL,
+	[Area] [nvarchar](50) NULL,
+	[country] [nchar](10) NULL,
+	[State] [nvarchar](50) NULL,
+	[agent] [bit] NULL,
+	[city] [nchar](10) NULL,
+	[DistrictId] [nchar](10) NULL,
+	[ToPrice] [nvarchar](50) NULL,
+ CONSTRAINT [PK_dbo.PropertyDetail] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+GO
+/****** Object:  Table [dbo].[PropertyImage]    Script Date: 5/23/2017 1:01:23 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[PropertyImage](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[PropertyTypeId] [int] NOT NULL,
+	[PropertyDetailId] [int] NOT NULL,
+	[PropertyImage] [varbinary](max) NOT NULL,
+	[Date] [datetime] NOT NULL,
+	[ImagePath] [nvarchar](50) NULL,
+	[IsActive] [bit] NOT NULL,
+ CONSTRAINT [PK_dbo.PropertyImage] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+GO
+/****** Object:  Table [dbo].[PropertyRequest]    Script Date: 5/23/2017 1:01:23 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[PropertyRequest](
+	[PropRequestId] [int] IDENTITY(1,1) NOT NULL,
+	[FullName] [nvarchar](50) NULL,
+	[Email] [nvarchar](50) NULL,
+	[PhoneNumber] [nvarchar](50) NULL,
+	[ContractType] [nvarchar](50) NULL,
+	[FromPrice] [nvarchar](50) NULL,
+	[Date] [datetime] NULL,
+	[AreaName] [nvarchar](50) NULL,
+	[Contact] [nvarchar](50) NULL,
+	[PropertyTypees] [nvarchar](50) NULL,
+	[PropertyDescription] [nvarchar](50) NULL,
+	[ToPrice] [nvarchar](50) NULL,
+	[AreaId] [nvarchar](50) NULL,
+	[CityId] [nvarchar](50) NULL,
+	[CountryId] [int] NULL,
+	[ContractId] [nvarchar](50) NULL,
+	[ContactBy] [nvarchar](50) NULL,
+ CONSTRAINT [PK_dbo.PropertyRequest] PRIMARY KEY CLUSTERED 
+(
+	[PropRequestId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[PropertyType]    Script Date: 5/23/2017 1:01:23 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[PropertyType](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+	[IsActive] [bit] NOT NULL,
+ CONSTRAINT [PK_dbo.PropertyType] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Roles]    Script Date: 5/23/2017 1:01:23 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Roles](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](256) NOT NULL,
+ CONSTRAINT [PK_dbo.Roles] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Service]    Script Date: 5/23/2017 1:01:23 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Service](
+	[Id] [int] NOT NULL,
+	[Name] [nvarchar](50) NULL,
+ CONSTRAINT [PK_dbo.Service] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[State]    Script Date: 5/23/2017 1:01:23 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[State](
+	[Id] [int] NOT NULL,
+	[Name] [nvarchar](50) NULL,
+	[IsActive] [bit] NULL,
+	[CountryId] [int] NOT NULL,
+ CONSTRAINT [PK_dbo.State] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[UserClaims]    Script Date: 5/23/2017 1:01:23 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[UserClaims](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[UserId] [int] NOT NULL,
+	[ClaimType] [nvarchar](max) NULL,
+	[ClaimValue] [nvarchar](max) NULL,
+ CONSTRAINT [PK_dbo.UserClaims] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[UserLogins]    Script Date: 5/23/2017 1:01:23 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[UserLogins](
+	[LoginProvider] [nvarchar](128) NOT NULL,
+	[ProviderKey] [nvarchar](128) NOT NULL,
+	[UserId] [int] NOT NULL,
+ CONSTRAINT [PK_dbo.UserLogins] PRIMARY KEY CLUSTERED 
+(
+	[LoginProvider] ASC,
+	[ProviderKey] ASC,
+	[UserId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[UserRoles]    Script Date: 5/23/2017 1:01:23 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[UserRoles](
+	[UserId] [int] NOT NULL,
+	[RoleId] [int] NOT NULL,
+ CONSTRAINT [PK_dbo.UserRoles] PRIMARY KEY CLUSTERED 
+(
+	[UserId] ASC,
+	[RoleId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Users]    Script Date: 5/23/2017 1:01:23 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Users](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [nvarchar](max) NULL,
+	[LastName] [nvarchar](max) NULL,
+	[Phone] [nvarchar](max) NULL,
+	[DateOfBirth] [datetime] NOT NULL,
+	[Email] [nvarchar](256) NULL,
+	[EmailConfirmed] [bit] NOT NULL,
+	[PasswordHash] [nvarchar](max) NULL,
+	[SecurityStamp] [nvarchar](max) NULL,
+	[PhoneNumber] [nvarchar](max) NULL,
+	[PhoneNumberConfirmed] [bit] NOT NULL,
+	[TwoFactorEnabled] [bit] NOT NULL,
+	[LockoutEndDateUtc] [datetime] NULL,
+	[LockoutEnabled] [bit] NOT NULL,
+	[AccessFailedCount] [int] NOT NULL,
+	[UserName] [nvarchar](256) NOT NULL,
+ CONSTRAINT [PK_dbo.Users] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+ALTER TABLE [dbo].[AgentInfo]  WITH CHECK ADD  CONSTRAINT [FK_dbo.AgentInfo_dbo.PropertyDetail_PropertyDetailId] FOREIGN KEY([PropertyDetailId])
+REFERENCES [dbo].[PropertyDetail] ([Id])
+GO
+ALTER TABLE [dbo].[AgentInfo] CHECK CONSTRAINT [FK_dbo.AgentInfo_dbo.PropertyDetail_PropertyDetailId]
+GO
+ALTER TABLE [dbo].[Amenity]  WITH CHECK ADD  CONSTRAINT [FK_dbo.Amenity_dbo.Feature_FeatureId] FOREIGN KEY([FeatureId])
+REFERENCES [dbo].[Feature] ([Id])
+GO
+ALTER TABLE [dbo].[Amenity] CHECK CONSTRAINT [FK_dbo.Amenity_dbo.Feature_FeatureId]
+GO
+ALTER TABLE [dbo].[Amenity]  WITH CHECK ADD  CONSTRAINT [FK_dbo.Amenity_dbo.Service_ServiceId] FOREIGN KEY([ServiceId])
+REFERENCES [dbo].[Service] ([Id])
+GO
+ALTER TABLE [dbo].[Amenity] CHECK CONSTRAINT [FK_dbo.Amenity_dbo.Service_ServiceId]
+GO
+ALTER TABLE [dbo].[Area]  WITH CHECK ADD  CONSTRAINT [FK_dbo.Area_dbo.District_DistrictId] FOREIGN KEY([DistrictId])
+REFERENCES [dbo].[District] ([Id])
+GO
+ALTER TABLE [dbo].[Area] CHECK CONSTRAINT [FK_dbo.Area_dbo.District_DistrictId]
+GO
+ALTER TABLE [dbo].[City]  WITH CHECK ADD  CONSTRAINT [FK_dbo.City_dbo.State_StateId] FOREIGN KEY([StateId])
+REFERENCES [dbo].[State] ([Id])
+GO
+ALTER TABLE [dbo].[City] CHECK CONSTRAINT [FK_dbo.City_dbo.State_StateId]
+GO
+ALTER TABLE [dbo].[Contact]  WITH CHECK ADD  CONSTRAINT [FK_dbo.Contact_dbo.ContractType_ContactTypeId] FOREIGN KEY([ContactTypeId])
+REFERENCES [dbo].[ContractType] ([Id])
+GO
+ALTER TABLE [dbo].[Contact] CHECK CONSTRAINT [FK_dbo.Contact_dbo.ContractType_ContactTypeId]
+GO
+ALTER TABLE [dbo].[District]  WITH CHECK ADD  CONSTRAINT [FK_dbo.District_dbo.City_CityId] FOREIGN KEY([CityId])
+REFERENCES [dbo].[City] ([Id])
+GO
+ALTER TABLE [dbo].[District] CHECK CONSTRAINT [FK_dbo.District_dbo.City_CityId]
+GO
+ALTER TABLE [dbo].[PropertyDetail]  WITH CHECK ADD  CONSTRAINT [FK_dbo.PropertyDetail_dbo.Amenity_AmenitiesID] FOREIGN KEY([AmenitiesID])
+REFERENCES [dbo].[Amenity] ([Id])
+GO
+ALTER TABLE [dbo].[PropertyDetail] CHECK CONSTRAINT [FK_dbo.PropertyDetail_dbo.Amenity_AmenitiesID]
+GO
+ALTER TABLE [dbo].[PropertyImage]  WITH CHECK ADD  CONSTRAINT [FK_dbo.PropertyImage_dbo.PropertyDetail_PropertyDetailId] FOREIGN KEY([PropertyDetailId])
+REFERENCES [dbo].[PropertyDetail] ([Id])
+GO
+ALTER TABLE [dbo].[PropertyImage] CHECK CONSTRAINT [FK_dbo.PropertyImage_dbo.PropertyDetail_PropertyDetailId]
+GO
+ALTER TABLE [dbo].[PropertyImage]  WITH CHECK ADD  CONSTRAINT [FK_dbo.PropertyImage_dbo.PropertyType_PropertyTypeId] FOREIGN KEY([PropertyTypeId])
+REFERENCES [dbo].[PropertyType] ([Id])
+GO
+ALTER TABLE [dbo].[PropertyImage] CHECK CONSTRAINT [FK_dbo.PropertyImage_dbo.PropertyType_PropertyTypeId]
+GO
+ALTER TABLE [dbo].[State]  WITH CHECK ADD  CONSTRAINT [FK_dbo.State_dbo.Country_CountryId] FOREIGN KEY([CountryId])
+REFERENCES [dbo].[Country] ([Id])
+GO
+ALTER TABLE [dbo].[State] CHECK CONSTRAINT [FK_dbo.State_dbo.Country_CountryId]
+GO
+ALTER TABLE [dbo].[UserClaims]  WITH CHECK ADD  CONSTRAINT [FK_dbo.UserClaims_dbo.Users_UserId] FOREIGN KEY([UserId])
+REFERENCES [dbo].[Users] ([Id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[UserClaims] CHECK CONSTRAINT [FK_dbo.UserClaims_dbo.Users_UserId]
+GO
+ALTER TABLE [dbo].[UserLogins]  WITH CHECK ADD  CONSTRAINT [FK_dbo.UserLogins_dbo.Users_UserId] FOREIGN KEY([UserId])
+REFERENCES [dbo].[Users] ([Id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[UserLogins] CHECK CONSTRAINT [FK_dbo.UserLogins_dbo.Users_UserId]
+GO
+ALTER TABLE [dbo].[UserRoles]  WITH CHECK ADD  CONSTRAINT [FK_dbo.UserRoles_dbo.Roles_RoleId] FOREIGN KEY([RoleId])
+REFERENCES [dbo].[Roles] ([Id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[UserRoles] CHECK CONSTRAINT [FK_dbo.UserRoles_dbo.Roles_RoleId]
+GO
+ALTER TABLE [dbo].[UserRoles]  WITH CHECK ADD  CONSTRAINT [FK_dbo.UserRoles_dbo.Users_UserId] FOREIGN KEY([UserId])
+REFERENCES [dbo].[Users] ([Id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[UserRoles] CHECK CONSTRAINT [FK_dbo.UserRoles_dbo.Users_UserId]
+GO
