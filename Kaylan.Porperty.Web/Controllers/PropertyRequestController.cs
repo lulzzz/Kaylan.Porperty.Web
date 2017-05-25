e6475b9591c765ae933e9bc815368c7337d9f820
harshal bhagat <harshalsbhagat@gmail.com>
2017-05-25 23:55:27 +0530

    Project Structure Changes

---
+++ Kaylan.Porperty.Web/Controllers/PropertyRequestController.cs
@@ -0,0 +1,47 @@
+﻿using Kalyan.Property.Infrastructure;
+using Kalyan.Property.Infrastructure.Models;
+using System.Data;
+using System.Web.Mvc;
+
+namespace Kaylan.Porperty.Web.Controllers
+{
+    public class PropertyRequestController : Controller
+    {
+        private IUnitOfWork unitOfWork;
+
+        // GET: PropertyRequest
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreatePropertyRequest()
+        {
+            return View(new PropertyRequest());
+        }
+
+        [HttpPost]
+        public ActionResult CreatePropertyRequest(PropertyRequest propertyrequest)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (unitOfWork = new UnitOfWork())
+                    {
+                        unitOfWork.Repository<PropertyRequest>().Add(propertyrequest);
+                        bool result = unitOfWork.Commit();
+                    }
+
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DataException)
+            {
+                ModelState.AddModelError("", "Unable to save changes. " +
+                  "Try again, and if the problem persists see your system administrator.");
+            }
+            return View(propertyrequest);
+        }
+    }
+}
