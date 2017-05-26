f228858412d58592afb5590e768e46a18acefc11
rupesh <rupesh.paikrao@gmail.com>
2017-05-26 15:57:17 +0530

    test

---
+++ Kaylan.Porperty.Web/Controllers/PropertyDetailController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kaylan.Porperty.Web.Controllers
+{
+    public class PropertyDetailController : Controller
+    {
+        // GET: PropertyDetail
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
