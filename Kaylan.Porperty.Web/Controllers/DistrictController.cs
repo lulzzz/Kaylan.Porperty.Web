f5bd9562fe6f4738e504c87351d30dec72bb8f01
rupesh <rupesh.paikrao@gmail.com>
2017-05-30 17:17:54 +0530

    changes in Admin.cshtml left Menu pages are added

--- Kaylan.Porperty.Web/Controllers/DistrictController.cs
+++ Kaylan.Porperty.Web/Controllers/DistrictController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Kalyan.Property.Infrastructure;
+using Kalyan.Property.Infrastructure.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +10,18 @@ namespace Kaylan.Porperty.Web.Controllers
 {
     public class DistrictController : Controller
     {
+
+        private IUnitOfWork unitOfWork;
+
+        public DistrictController()
+        {
+            unitOfWork = new UnitOfWork();
+        }
+
+
+
+
+
         // GET: District
         public ActionResult CreateDistrict()
         {
@@ -17,5 +31,26 @@ namespace Kaylan.Porperty.Web.Controllers
         {
             return View();
         }
+
+        public ActionResult GetState(int stateId)
+        {
+            try
+            {
+                List<State> result = new List<State>();
+
+                result = unitOfWork.Repository<State>().GetMany(x => x.Id == stateId).ToList();
+
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception exception)
+            {
+                System.Diagnostics.Debug.WriteLine(exception);
+                return null;
+            }
+        }
+
+
+
+
     }
 }
--- Kaylan.Porperty.Web/Controllers/PropertyDetailController.cs
+++ Kaylan.Porperty.Web/Controllers/PropertyDetailController.cs
@@ -13,5 +13,6 @@ namespace Kaylan.Porperty.Web.Controllers
         {
             return View();
         }
+        
     }
 }
