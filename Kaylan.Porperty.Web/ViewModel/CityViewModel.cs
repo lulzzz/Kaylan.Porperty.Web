d8cfb6a4fef291c1b01a9d0e6fb6bbca2b40c14c
PrituT <pritutripathi1002@gmail.com>
2017-06-01 21:51:10 +0530

    Remove comments

--- Kaylan.Porperty.Web/ViewModel/CityViewModel.cs
+++ Kaylan.Porperty.Web/ViewModel/CityViewModel.cs
@@ -5,13 +5,7 @@ namespace Kaylan.Porperty.Web.ViewModel
 {
     public class CityViewModel
     {
-        //public IPagedList<City> CityList { get; internal set; }
-
-        //public string CityName { get; internal set; }
-
-        //public int CountryId { get; internal set; }
-
-        //public int StateId { get; internal set; }
+        
         public IPagedList<City> CityList { get;  set; }
 
         public string CityName { get;  set; }
