a407e9333d96da3fde85d9a475a7c21e5f43b6c2
harshal bhagat <harshalsbhagat@gmail.com>
2017-06-02 23:38:22 +0530

    Register and Login page are completed

---
+++ Kaylan.Porperty.Web/ViewModel/UserViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Kaylan.Porperty.Web.ViewModel
+{
+    public class UserViewModel
+    {
+        [Required(ErrorMessage = "First Name Required")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name Required")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Phone number Required")]
+        [Display(Name = "Phone number")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Gender Required")]
+        [Display(Name = "Gender")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "User Name Required")]
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Date Of Birth Required")]
+        [Display(Name = "Date Of Birth")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "First Name Required")]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
